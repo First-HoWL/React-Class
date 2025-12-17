bf52e45f56c67c1ead911dba1fbf31a34a876e65
HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-12-17 18:17:57 +0000

    button is completed

--- src/App.js
+++ src/App.js
@@ -1,155 +1,6 @@
 import { useState } from "react";
 import "./styles.css";
 
-function ClickerFeature({ firstButton, secButton, thirdButton }) {
-  const [clicked, setclick] = useState(0);
-
-  function addclicks(inp) {
-    setclick(clicked + parseInt(inp));
-  }
-
-  return (
-    <>
-      <div className="counter">
-        <button
-          onClick={() => {
-            addclicks(firstButton);
-          }}
-        >
-          {firstButton}
-        </button>
-        <button
-          onClick={() => {
-            addclicks(secButton);
-          }}
-        >
-          {secButton}
-        </button>
-        <button
-          onClick={() => {
-            addclicks(thirdButton);
-          }}
-        >
-          {thirdButton}
-        </button>
-        <span>{clicked}</span>
-      </div>
-    </>
-  );
-}
-
-function Note({ profile }) {
-  // let isEdit = true;
-  const [isEditName, setisEditName] = useState(false);
-  const [isEditEmail, setisEditEmail] = useState(false);
-  const [profile1, setprofile] = useState(profile);
-  const [isEditselect1, setisEditselect1] = useState(false);
-
-  return (
-    <>
-      <h3>Notes</h3>
-      {isEditName ? (
-        <form
-          onSubmit={() => {
-            setisEditName(false);
-          }}
-        >
-          <textarea
-            onChange={(e) => {
-              setprofile({
-                name: e.target.value,
-                email: profile1.email,
-                gender: profile1.gender,
-              });
-            }}
-            required
-            value={profile1.name}
-          ></textarea>
-          <button type="submit">Save</button>
-        </form>
-      ) : (
-        <>
-          <p>{profile1.name}</p>
-          <button
-            onClick={() => {
-              setisEditName(true);
-            }}
-          >
-            Edit
-          </button>
-        </>
-      )}
-      {isEditEmail ? (
-        <form
-          onSubmit={() => {
-            setisEditEmail(false);
-          }}
-        >
-          <textarea
-            onChange={(e) => {
-              setprofile({
-                name: profile1.name,
-                email: e.target.value,
-                gender: profile1.gender,
-              });
-            }}
-            required
-            value={profile1.email}
-          ></textarea>
-          <button type="submit">Save</button>
-        </form>
-      ) : (
-        <>
-          <p>{profile1.email}</p>
-          <button
-            onClick={() => {
-              setisEditEmail(true);
-            }}
-          >
-            Edit
-          </button>
-        </>
-      )}
-
-      {isEditselect1 ? (
-        <form
-          onSubmit={() => {
-            setisEditselect1(false);
-          }}
-        >
-          <select
-            onChange={(event) => {
-              setprofile({
-                name: profile1.name,
-                email: profile1.email,
-                gender: event.target.value,
-              });
-            }}
-            value={profile1.gender}
-          >
-            <option value="Man"> Man </option>
-            <option value="Woman"> Woman </option>
-            <option value="None"> None </option>
-            <option value="Croissant"> Croissant </option>
-          </select>
-          <button type="submit">Save</button>
-        </form>
-      ) : (
-        <>
-          <p>{profile1.gender}</p>
-          <button
-            onClick={() => {
-              setisEditselect1(true);
-            }}
-          >
-            Edit
-          </button>
-        </>
-      )}
-    </>
-  );
-}
-
 function PersonalSite({ profile }) {
   return (
     <>
@@ -167,36 +18,40 @@ function PersonalSite({ profile }) {
 }
 
 function ThemeSwitch({ onClick }) {
-  const [ispressed, setIsPressed] = useState(false);
+  const [ispressed, setIsPressed] = useState(true);
   return (
-    <div class="radButton">
-      <button>Switch theme</button>
+    <div
+      onClick={() => {
+        setIsPressed(ispressed ? false : true);
+        onClick(ispressed);
+      }}
+      className={ispressed ? "radButton" : "radButton on"}
+    >
+      <div className="circleBtn"></div>
     </div>
   );
 }
 
 export default function App() {
   const [theme, setTheme] = useState("");
-  const [isPressed, setIsPressed] = useState("");
+  const [isPressed, setIsPressed] = useState(false);
   return (
     <>
-      <main className={theme}>
+      <main /*className={isPressed ? "darkTheme" : ""}*/>
         <ThemeSwitch onClick={(pressed) => setIsPressed(pressed)} />
         <h2>Hello World!</h2>
+        <input type="radio" checked={isPressed}></input>
         <p>
-          {theme == ""
+          {/* {theme == ""
             ? "White Theme"
             : theme == "darkTheme"
             ? "Dark Theme"
             : theme == "greenTheme"
             ? "Green Theme"
-            : "Blue Theme"}
+            : "Blue Theme"} */}
         </p>
       </main>
 
-      {/* <Note
-        profile={{ name: "HoWL", email: "HoWL@gmail.com", gender: "Man" }}
-      /> */}
       {/* <PersonalSite
         profile={{
           firstName: "Denis",
